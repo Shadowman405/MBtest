409169862d8a4cc1c0c6abf520ec375aba53ff25
Shadowman405 <maximus405@gmail.com>
2023-10-02 22:47:28 +0300

    Initial Commit

---
+++ MBtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBtest
+//
+//  Created by Maxim Mitin on 2.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
