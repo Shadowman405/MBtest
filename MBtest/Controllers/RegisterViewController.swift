0c90f07d55911171e26ff01c2a4605f4f04a3c2e
Shadowman405 <maximus405@gmail.com>
2023-10-03 22:14:22 +0300

    configure registerVC

---
+++ MBtest/Controllers/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  MBtest
+//
+//  Created by Maxim Mitin on 3.10.23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var registerBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupUI(){
+        emailTxtFld.placeholder = "Email"
+        passwordTxtFld.placeholder = "Password"
+        registerBtn.layer.cornerRadius = 15
+    }
+    
+    @IBAction func registerTaped(_ sender: Any) {
+    }
+    
+
+}
